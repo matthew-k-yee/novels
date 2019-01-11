9d8a5097453cfbcf63e4b1de4444db60cf90deba
Matthew Yee <matthewyee@ool-ad020572.dyn.optonline.net>
2019-01-11 15:58:32 -0500

    fixing readme

--- novels/db/schema.rb
+++ novels/db/schema.rb
@@ -1,4 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
+A# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2019_01_10_164032) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password_digest"
+    t.string "f"
     t.string "username"
     t.string "firstname"
     t.string "lastname"
